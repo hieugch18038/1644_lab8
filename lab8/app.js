f0bc570712bd3085bacc4e25e908a290ed3f056b
Nguyen Huy Hieu <hieunhgch18038@gmail.com>
2022-10-01 15:24:42 +0700

    hy

--- lab8/app.js
+++ lab8/app.js
@@ -3,6 +3,17 @@ const app = express();
 const port = process.env.PORT || 3000;
 const path = require("path");
 
+const mongoose = require("mongoose");
+const studentModel = require("./models/StudentSchema");
+const url = "mongodb://localhost:27017/greenwich";
+mongoose.connect(url, { useNewUrlParser: true }, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("connect to db succeed !");
+  }
+});
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
 
@@ -12,6 +23,21 @@ app.get("/", (req, res) => {
 
 app.get("/about", function (req, res) {
   res.render("about");
+});
+
+app.get("/student", (req, res) => {
+    studentModel.find((err, data) => {
+        if (err) {
+            console.log(err);
+        }else {
+            //1. show dữ liệu ra console log
+            //console.log(data);
+            //2. show dữ liệu ra web bằng "send"
+            //res.send(data);
+            //3. show dữ liệu ra view bằng "render"
+            res.render("student", {students: data});
+        }
+    });
 });
 
 app.listen(port);
---
+++ lab8/models/StudentSchema.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+var studentSchema = mongoose.Schema({
+    name: String,
+    age: Number,
+    email: String,
+    image: String,
+});
+
+var studentModel = mongoose.model("student", studentSchema, "student");
+
+module.exports = studentModel;
